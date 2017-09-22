3e95e6f512641ed2a5ff06b43c7d64c5ef5de83a
dpalmer9 <dpalmer9@uwo.ca>
2017-09-22 12:18:59 -0400

    Additional Development

--- PAL Processor Script.R
+++ PAL Processor Script.R
@@ -65,47 +65,47 @@ bad.list$bad.main4 = c() #32
 
 # Good Names #
 good.names = vector()
-good.names[1] = ''
-good.names[2] = ''
-good.names[3] = ''
-good.names[4] = ''
-good.names[5] = ''
+good.names[1] = 'Site1'
+good.names[2] = 'Site2'
+good.names[3] = 'APP-PS1'
+good.names[4] = '5xFAD'
+good.names[5] = '3xTG-AD'
 good.names[6] = NA
 good.names[7] = NA
-good.names[8] = ''
-good.names[9] = ''
-good.names[10] = ''
-good.names[11] = ''
-good.names[12] = ''
-good.names[13] = ''
-good.names[14] = ''
-good.names[15] = ''
-good.names[16] = ''
-good.names[17] = ''
-good.names[18] = ''
-good.names[19] = ''
-good.names[20] = ''
-good.names[21] = ''
-good.names[22] = ''
-good.names[23] = ''
-good.names[24] = ''
-good.names[25] = ''
-good.names[26] = ''
-good.names[27] = ''
-good.names[28] = ''
-good.names[29] = ''
-good.names[30] = ''
-good.names[31] = ''
-good.names[32] = ''
+good.names[8] = 'C57BL6'
+good.names[9] = 'APPPS1'
+good.names[10] = 'B6SJLF1/J'
+good.names[11] = '5xFAD'
+good.names[12] = 'B6129SF2/J'
+good.names[13] = '3xTG-AD'
+good.names[14] = 'Female'
+good.names[15] = 'Male'
+good.names[16] = '4'
+good.names[17] = '7'
+good.names[18] = '10'
+good.names[19] = NA
+good.names[20] = NA
+good.names[21] = "Phase I"
+good.names[22] = 'Phase II'
+good.names[23] = 'Phase III'
+good.names[24] = 'Phase IV'
+good.names[25] = 'dPAL Acquisition 1'
+good.names[26] = 'dPAL Acquisition 2'
+good.names[27] = 'dPAL Acquisition 3'
+good.names[28] = 'sPAL Acquisition 1'
+good.names[29] = 'dPAL1'
+good.names[30] = 'dPAL2'
+good.names[31] = 'dPAL3'
+good.names[32] = 'dPAL4'
 
 # Column Match to Name Check #
 lookup.list = list()
-lookup.list$site.col.lookup = c(1:2)
-lookup.list$strain.col.lookup = c(3:5)
-lookup.list$genotype.col.lookup = c(6:13)
-lookup.list$sex.col.lookup = c(14:15)
-lookup.list$age.col.lookup = c(16:18)
-lookup.list$protocol.col.lookup = c(19:32)
+lookup.list$TestSite = c(1:2)
+lookup.list$Mouse.Strain = c(3:5)
+lookup.list$Genotype = c(6:13)
+lookup.list$Sex = c(14:15)
+lookup.list$Age.Months = c(16:18)
+lookup.list$Task = c(19:32)
 
 #######################
 
@@ -116,7 +116,7 @@ Name.Search = function(x, lookup.list=lookup.list, good.names=good.names, bad.na
   good.name = ''
   bad.names = c()
   for(a in 1:length(lookup.list)){
-    if(isTRUE(x %in% unlist(lookup.list[a])){
+    if(isTRUE(x %in% unlist(lookup.list[a]))){
       
     }
   }
@@ -132,7 +132,7 @@ QC.Main.Function
 
 # Fix Naming Rules - Main & Acq #
 Naming.MainAcq.Function = function(dataset, lookup.list=lookup.list, good.names=good.names, bad.names=bad.names){
-  colnames(dataset)[1:11] = c('Database','AnimalID','TestSite','Mouse.Strain','Genotype','Gender','Age.Months','Task','Date','Day','Week')
+  colnames(dataset)[1:11] = c('Database','AnimalID','TestSite','Mouse.Strain','Genotype','Sex','Age.Months','Task','Date','Day','Week')
   # Fix Spacing on relevant Columns #
   col.list.spacefix = c(1,2,3,4,5,6,7,10,11)
   for(a in col.list.spacefix){
@@ -147,7 +147,13 @@ Naming.MainAcq.Function = function(dataset, lookup.list=lookup.list, good.names=
     position.vec = c(1:5,6:7,14:32)
   }
   for(a in position.vec){
-    check.data = Name.Search(a)
+    if((a <= 1) & (a >= 2)){
+      col.num = 3
+    }else if((a <=3) & (a >= 5)){
+      
+    }else if((a <=6) & (a >= 13)){
+      
+    }else if((a <=14) & (a >= 15))
     
   }
   
