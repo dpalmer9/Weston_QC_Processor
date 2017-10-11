bb3ea056333e7fb490c6c77c0bc035afc7972a15
dpalmer9 <dpalmer9@uwo.ca>
2017-10-11 12:51:39 -0400

    Update 5CSRTT Script

--- 5CSRTT Processor Script.R
+++ 5CSRTT Processor Script.R
@@ -512,7 +512,9 @@ Vigilance.Calc.Function = function(dataset,binsize){
   bin.num = 1
   for(a in 1:total.bins){
     acc.col = a
+    colnames(new.data)[acc.col] = paste("Accuracy.Block",a,sep=".")
     om.col = a + total.bins
+    colnames(new.data)[om.col] = paste("Omission.Block",a,sep=".")
     for(b in 1:nrow(dataset)){
       acc.list = as.vector(dataset[b,c(acc.start:(acc.start + binsize - 1))])
       om.list = as.vector(dataset[b,c(omission.start:(omission.start + binsize - 1))])
@@ -522,14 +524,14 @@ Vigilance.Calc.Function = function(dataset,binsize){
         start.acc = 0
         start.om = 0
       }else{
-        start.acc = dataset[b,(acc.start - 1)]
-        start.om = dataset[b,(omission.start - 1)]
+        start.acc = as.numeric(dataset[b,(acc.start - 1)])
+        start.om = as.numeric(dataset[b,(omission.start - 1)])
       }
       acc.track = c()
       om.track = c()
       for(c in 1:(binsize)){
-        curr.acc = acc.list[c]
-        curr.om = om.list[c]
+        curr.acc = as.numeric(acc.list[c])
+        curr.om = as.numeric(om.list[c])
         if(isTRUE(curr.acc > start.acc)){
           acc.bin = acc.bin + 1
         }else if(isTRUE(curr.om > start.om)){
@@ -538,13 +540,16 @@ Vigilance.Calc.Function = function(dataset,binsize){
         start.acc = curr.acc
         start.om = curr.om
       }
-      acc.bin = acc.bin / (binsize - om.bin)
-      om.bin = om.bin / binsize
+      acc.bin = (acc.bin / (binsize - om.bin)) * 100
+      om.bin = (om.bin / binsize) * 100
       new.data[b,acc.col] = acc.bin
       new.data[b,om.col] = om.bin
     }
+    acc.start = (acc.start + binsize)
+    omission.start = omission.start + binsize
   }
-  return(new.data)
+  final.data = cbind(dataset[ ,1:12],new.data)
+  return(final.data)
 }
 
 ############################################################################################
